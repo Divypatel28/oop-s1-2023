bc170664fef0fda7f29f735b686ea26885e8144e
Divy Patel <divypatel@192-168-1-7.tpgi.com.au>
2023-07-28 20:18:08 +0930

    main1.2

--- practical1/function-1-1.cpp
+++ practical1/function-1-1.cpp
@@ -12,7 +12,3 @@ int array_sum(int array[], int n) {
     return sum;
 }
 
-int main(){
-    int array[] = {1,2,3};
-    std::cout<<array_sum(array,2)<<std::endl;
-}
--- practical1/main-1-1.cpp
+++ practical1/main-1-1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+
+extern int array_sum(int array[], int n);
+
 int main(){
     int array[] = {1,2,3};
-    std::cout<<array_sum(array,2)<<std::endl;
+    std::cout<< "The sum is: "<< array_sum(array,2)<<std::endl;
 }
