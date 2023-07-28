ea7347bb0fcaa2b41a59dac2cce5dbcbfc3ae632
Divy Patel <divypatel@192-168-1-7.tpgi.com.au>
2023-07-28 19:30:50 +0930

    Question 1.2

---
+++ practical1/function-1-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+double array_mean(int array[], int n) {
+    if (n<1){
+        return 0.0;
+    }
+    int sum=0;
+    for (int i = 0; i < n; i++) {
+    sum += array[i];
+}
+ double avg_num = (double)sum / n;
+  return avg_num;
+}
