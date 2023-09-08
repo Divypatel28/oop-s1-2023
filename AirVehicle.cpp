2c2be42a97c3643ab8f4004018c42244ea080d99
Divypatel28 <140474621+Divypatel28@users.noreply.github.com>
2023-09-08 16:31:00 +0930

    Add files via upload

---
+++ AirVehicle.cpp
@@ -0,0 +1,33 @@
+#include "AirVehicle.h"
+
+AirVehicle::AirVehicle() {
+    weight = 0;
+    fuel = 100.0f;
+    numberOfFlights = 0;
+}
+
+AirVehicle::AirVehicle(int w) {
+    weight = w;
+    fuel = 100.0f;
+    numberOfFlights = 0;
+}
+
+
+int AirVehicle::get_weight() const {
+    return weight;
+}
+void AirVehicle::set_weight(int w) {
+    weight = w;
+}
+float AirVehicle::get_fuel() const {
+    return fuel;
+}
+int AirVehicle::get_numberOfFlights() const {
+    return numberOfFlights;
+}
+void AirVehicle::refuel() {
+    fuel = 100.0f;
+}
+void AirVehicle::fly(int headwind, int minutes) {
+    numberOfFlights++;
+}
