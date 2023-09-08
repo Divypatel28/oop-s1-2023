bbbcc4cb4e264cbc6217b60047e3a6cc2ed16d6c
Divypatel28 <140474621+Divypatel28@users.noreply.github.com>
2023-09-08 16:34:28 +0930

    Add files via upload

---
+++ AirVehicle.h
@@ -0,0 +1,28 @@
+#ifndef AIRVEHICLE_H
+#define AIRVEHICLE_H
+
+class AirVehicle {
+public:
+    AirVehicle();
+    AirVehicle(int w);
+
+
+    int get_weight() const;
+    void set_weight(int w);
+
+
+    float get_fuel() const;
+
+    int get_numberOfFlights() const;
+
+
+    void refuel();
+    virtual void fly(int headwind, int minutes);
+
+private:
+    int weight;
+    float fuel;
+    int numberOfFlights;
+};
+
+#endif
