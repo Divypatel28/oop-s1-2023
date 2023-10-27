6796146ae574f3fe6a3ab9f4d4f3836a3f7adad0
Divypatel28 <140474621+Divypatel28@users.noreply.github.com>
2023-10-27 16:58:06 +1030

    Add files via upload

--- main-3.cpp
+++ main-3.cpp
@@ -1,12 +1,11 @@
 #include "Play.h"
 
 int main() {
-    
     Play game;
 
-    game.initPlay(2, 4, 20, 20);  // 2 persona, 5 snares, in a 15x15 matrix
-
-    game.playCycle(30, 4.0);  // Limit of number of cycles is 20, Snare dist: 4.0 Units
+    game.playCycle(15, 2.0);  
+    
+    game.initPlay(1, 2, 10, 10);
 
     return 0;
 }
