bc95de2ef7cfd6cf5a0a28cacc48f70532972a30
Divypatel28 <140474621+Divypatel28@users.noreply.github.com>
2023-10-27 17:03:02 +1030

    reuploading final copies

--- Play.h
+++ Play.h
@@ -2,63 +2,75 @@
 #define PLAY_H
 #include <vector>
 #include <iostream>
-#include "Spot.h"
-#include "Persona.h"
 #include "Snare.h"
+#include "Persona.h"
 #include "Assists.h"
-//including all needed classes and libraries from previous questions 1 and 2
-//Play.h
+#include "Spot.h"
+
 
 class Play {
 
 private:
+
     std::vector<Spot*> matrix;
 
 
+
 public:
+
     Play() {}
+
     std::vector<Spot*>& getMatrix() { 
 
         return matrix;
     }
 
-    void initPlay(int numPersonas, int numSnares, int matrixHeight, int matrixWidth) {
+    void initPlay(int numPersonas, int numSnares, int matrixWidth, int matrixHeight) {
+
         matrix.clear();
+
+        for (int i = 0; i < numPersonas; ++i) {
+
+            auto loc = Assists::createRandomLoc(matrixWidth, matrixHeight);
+
+            matrix.push_back(new Persona(std::get<0>(loc), std::get<1>(loc)));
+
+        }
 
 
         for (int i = 0; i < numSnares; ++i) {
+
             auto loc = Assists::createRandomLoc(matrixWidth, matrixHeight);
+
             matrix.push_back(new Snare(std::get<0>(loc), std::get<1>(loc)));
         }
-
-
-        for (int i = 0; i < numPersonas; ++i) {
-            auto loc = Assists::createRandomLoc(matrixWidth, matrixHeight);
-            matrix.push_back(new Persona(std::get<0>(loc), std::get<1>(loc)));
-        }
-
     }
 
+
        void playCycle(int maxCycles, double snareTriggerDistance) {
-    
+
         int cycles = 0;
+
         while (cycles < maxCycles) {
             for (Spot* spot : matrix) {
                 if (spot->getCategory() == 'P') {
                     Persona* persona = static_cast<Persona*>(spot);
                     persona->shift(1, 0);
 
+                    
                     if (std::get<0>(persona->getLoc()) > static_cast<int>(matrix.size())) {
+                        // as output requirement from questions
                         std::cout << "Persona has won the game!" << std::endl;
+
                         return;
                     }
                 }
             }
-
             cycles++;
         }
+        // as output requirement from questions
+        std::cout << "Maximum number of cycles reached. Game over." << std::endl;
 
-        std::cout << " Maximum number of cycles reached. Game over. " << std::endl;
     }
 };
 
